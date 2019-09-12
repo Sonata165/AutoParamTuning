d9a7c38ff6b5bb9b9e780fc53f1a49d38ef443d8
zkx741481546 <zkx741481546@live.com>
2019-09-12 20:07:44 +0800

    部分完成，测试push

--- knowledge/KnowledgePrepare.py
+++ knowledge/KnowledgePrepare.py
@@ -10,14 +10,90 @@ database目录结构如README中所示
 	第一列不是编号，从第一列开始就是数据
 '''
 
-def get_param_name(alg_name):
-	'''
-	返回待调的超参数的名称
-	Parameters:
-	  alg_name - String类型，待调参算法名称，'SVM','ElasticNet','GMM'中的一个
-	Returns:
-	  一个列表，包含该算法要调的参数的名称。
-	  要求列表大小和神经网络输出数目总数保持一致，顺序也和神经网络输出保持一致
-	'''
-	return ['a']
+from system.FeatureCalc import calculate_features
+import os
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.linear_model import ElasticNet
+from sklearn.mixture import GaussianMixture
+import numpy as np
 
+
+def get_feature(modelName, param_g=None):
+    """
+    获取特征-标签数据
+    返回最优参数的同时保存数据，格式为：
+          feature1  feature2 ... best_param(label)
+    name:
+    data:
+    :param modelName: 要优化的模型，字符串，区分大小写，例：'SVM','ElasticNet','GaussianMixture
+    :param param_g: 网格搜索范围， 默认值为None，并使用内置的搜索范围，格式如下：
+                    例:SVM
+                    {'kernel': ['linear', 'rbf', 'poly', 'sigmoid', 'precomputed'], 'C': [1, 10, 50, 10]}
+    :return: 所有特征的列表
+    """
+    # 搜索精度
+    accuracy = 1000
+    database_dir = '../database/' + modelName
+    dir_list = os.listdir(database_dir)
+    feature = []
+    for p in dir_list:
+        path = os.path.join(database_dir, p)
+        print("path: " + path)
+        # 读取数据
+        data = pd.read_csv(path)
+        # 计算特征
+        feature.append(calculate_features(data))
+        # 保存特征
+        pd.DataFrame(feature).to_csv(p + '_feature.csv')
+        # TODO:网格搜索数据准备
+        x_data = np.array(data)[, :-1]
+        y_data = np.array(data)[, -1:]
+        # 网格搜索标签
+        if modelName == "SVM":
+            # 生成搜索范围
+            r = [i / accuracy for i in range(-10 * accuracy, 10 * accuracy, 1)]
+            if param_g is None:
+                param_grid = {'kernel': ['linear', 'rbf', 'poly', 'sigmoid', 'precomputed'], 'C': r,
+                              'gamma': r}
+            else:
+                param_grid = param_g
+            model = SVC()
+            gs = GridSearchCV(model, param_grid=param_grid, refit=True, cv=5).fit(x_data, y_data)
+
+
+        elif modelName == 'ElasticNet':
+            param_grid = {'alpha': [0.01, 0.1, 1, 10, 100], 'l1_ratio': [0, 0.01, 0.05, 0.1, 0.5, 1]}
+            model = ElasticNet()
+            gs = GridSearchCV(model, param_grid=param_grid, refit=True, cv=5).fit(x_data, y_data)
+
+        elif modelName == 'GMM':
+            param_grid = {'n_components': [1, 2, 3], 'covariance': ['full', 'tied', 'diag', 'spherical']}
+            model = GaussianMixture()
+            gs = GridSearchCV(model, param_grid=param_grid, refit=True, cv=5).fit(x_data, y_data)
+
+        else:
+            print("模型名称输入错误！")
+            return None
+        # 输出网格搜索结果
+        print("best_params:\n" + str(gs.best_params_))
+        print('best_score:\n' + str(gs.best_score_))
+
+        path = os.path.join('/' + modelName + '/', p)
+        # TODO:处理结果并保存
+
+        # TODO:确定feature的类型并转为list
+        return feature
+
+
+def get_param_name(alg_name):
+    '''
+    返回待调的超参数的名称
+    Parameters:
+      alg_name - String类型，待调参算法名称，'SVM','ElasticNet','GMM'中的一个
+    Returns:
+      一个列表，包含该算法要调的参数的名称。
+      要求列表大小和神经网络输出数目总数保持一致，顺序也和神经网络输出保持一致
+    '''
+    return ['a']
--- knowledge/NetworkPrepare.py
+++ knowledge/NetworkPrepare.py
@@ -2,3 +2,137 @@
 使用KnowledgePrepare生成的三个csv文件训练神经网络，
 将训练好的模型保存至system/network下的三个.h5文件中，请用keras的model.save('路径名')
 '''
+
+from keras.layers import *
+from keras import Model
+import keras
+
+
+def reg_net(input_shape):
+    """
+    生成单输出回归神经网
+    :param input_shape: 输入维度，元组
+    :return: compile好的Keras模型
+    """
+    x_input = Input(input_shape)
+    x = Dense_withBN_Dropout(x_input, 16)
+    x = Dense_withBN_Dropout(x, 16)
+    x = Dense_withBN_Dropout(x, 4)
+    x = Dense_withBN_Dropout(x, 1)
+    model = Model(inputs=[x_input], outputs=[x])
+    model.compile(
+        loss=keras.losses.mean_squared_error,
+        optimizer=keras.optimizers.Adam(0.001),  # 学习率初始值为0.001
+        metrics=['mae', 'mse']  # 评估指标: [平均绝对误差, 均方误差]
+    )
+    return model
+
+
+def build_SVM_Kernel_nn(input_shape, output_dim):
+    """
+    生成预测SVM超参数Kernel的神经网
+    :param input_shape: 输入维度，元组，如有6个feature，则input_shape=(6,)
+    :param output_dim: 输出维度，int，分类问题中的总类别数，如有4种核函数，则output=4
+                        输出如下：[0,1,0,0]，值为1的为预测的kernel，列表按kernel名称的字典序排列
+    :return: compile好的keras模型
+    """
+    x_input = Input(input_shape)
+    x = Dense_withBN_Dropout(x_input, 16)
+    x = Dense_withBN_Dropout(x, 16)
+    x = Dense_withBN_Dropout(x, 4)
+    x = Dense_withBN_Dropout(x, output_dim, activation='softmax')
+    model = Model(inputs=[x_input], outputs=[x])
+    model.compile(
+        loss=keras.losses.categorical_crossentropy,
+        optimizer=keras.optimizers.Adam(0.001),  # 学习率初始值为0.001
+        metrics=['mae', 'mse']  # 评估指标: [平均绝对误差, 均方误差]
+    )
+    return model
+
+
+def build_SVM_C_nn(input_shape):
+    """
+    生成预测SVM超参数C的神经网
+    :param input_shape: 输入维度，元组，如有6个feature，则input_shape=(6,)
+    :return: compile好的Keras模型
+    """
+    return reg_net(input_shape)
+
+
+def build_SVM_gamma_nn(input_shape):
+    """
+    生成预测SVM超参数gamma的神经网
+    :param input_shape: 输入维度，元组，如有6个feature，则input_shape=(6,)
+    :return: compile好的Keras模型
+    """
+    return reg_net(input_shape)
+
+
+def build_ElasticNet_alpha_nn(input_shape):
+    """
+    生成预测ElasticNet超参数alpha的神经网
+    :param input_shape: 输入维度，元组，如有6个feature，则input_shape=(6,)
+    :return: compile好的Keras模型
+    """
+    return reg_net(input_shape)
+
+
+def build_ElasticNet_l1ratio_nn(input_shape):
+    """
+    生成预测ElasticNet超参数alpha的神经网
+    :param input_shape: 输入维度，元组，如有6个feature，则input_shape=(6,)
+    :return: compile好的Keras模型
+    """
+    return reg_net(input_shape)
+
+
+
+
+
+
+def Dense_withBN_Dropout(input, units, activation=None):
+    """
+    全连接-BN层-激活层-Dropout层的神经元模块
+    :param input: 输入
+    :param units: 全连接层神经元个数
+    :param activation: 默认为None，则采用LeakyRelu激活函数，否则应传入Keras中激活函数的名字，如'softmax'
+    :return: tensor，神经元输出
+    """
+    x = Dense(units=units)(input)
+    x = BatchNormalization()(x)
+    if activation is None:
+        x = LeakyReLU(alpha=0.3)(x)
+    else:
+        x = Activation(activation)(x)
+    x = Dropout(rate=0.1)(x)
+    return x
+
+
+class PrintDot(keras.callbacks.Callback):  # 一个回调函数
+    def on_epoch_end(self, epoch, logs):
+        rnd = epoch + 1
+        if rnd % 5 == 0:
+            print('.', end='')
+        if rnd % 100 == 0:
+            print('')
+
+
+def train_nn(model, x_train, y_train, epochs, model_name):
+    """
+    训练神经网，保存神经网，返回history
+    :param model: 要训练的模型
+    :param x_train: x
+    :param y_train: y   形式:[c,gamma]
+    :param epochs: 起始训练轮数
+    :param model_name: 拟合的算法名字
+    :return: history
+    """
+    early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)  # 用EarlyStopping创建另一个回调函数
+    history = model.fit(x=x_train, y=y_train, epochs=epochs, validation_split=0.2, callbacks=[early_stop, PrintDot()])
+    model.save('../system/network/' + model_name + '.h5')
+    return history
+
+
+def plot_history(history):
+    # TODO
+    pass
