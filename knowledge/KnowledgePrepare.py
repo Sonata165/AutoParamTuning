2423f9c5e730a787bbb166f3f71ffae40b63ee8f
1173710224 <40821066+1173710224@users.noreply.github.com>
2019-10-07 17:41:39 +0800

    Update KnowledgePrepare.py

--- knowledge/KnowledgePrepare.py
+++ knowledge/KnowledgePrepare.py
@@ -1,24 +1,281 @@
-'''
-计算database目录下所有数据集的特征和最优参数（网格搜索）
-database目录结构如README中所示
-计算特征时，可以调用system/FeatureCalc.py中的calculate_features(dataset)，
-其中dataset是一个读入的数据集（用read_csv函数读入，类型为DataFrame），返回值是所有特征的列表
-计算最优参数时，可以参考原来的old/compute_label.py
-全部计算完后，将结果保存在如README所示knowledge下三个csv文件中
-保存格式如下：
-	需要把最优参数放在后面的列，特征放在前面的列
-	第一行是特征名和参数名，从第二行开始是数据
-	第一列不是编号，从第一列开始就是数据
-'''
+#'''
+#计算database目录下所有数据集的特征和最优参数（网格搜索）
+#database目录结构如README中所示
+#计算特征时，可以调用system/FeatureCalc.py中的calculate_features(dataset)，
+#其中dataset是一个读入的数据集（用read_csv函数读入，类型为DataFrame），返回值是所有特征的列表
+#计算最优参数时，可以参考原来的old/compute_label.py
+#全部计算完后，将结果保存在如README所示knowledge下三个csv文件中
+#保存格式如下：
+#	需要把最优参数放在后面的列，特征放在前面的列
+#	第一行是特征名和参数名，从第二行开始是数据
+#	第一列不是编号，从第一列开始就是数据
+#'''
+#
+#def get_param_name(alg_name):
+#	'''
+#	返回待调的超参数的名称
+#	Parameters:
+#	  alg_name - String类型，待调参算法名称，'SVM','ElasticNet','GMM'中的一个
+#	Returns:
+#	  一个列表，包含该算法要调的参数的名称。
+#	  要求列表大小和神经网络输出数目总数保持一致，顺序也和神经网络输出保持一致
+#	'''
+#	return ['a']
 
-def get_param_name(alg_name):
-	'''
-	返回待调的超参数的名称
-	Parameters:
-	  alg_name - String类型，待调参算法名称，'SVM','ElasticNet','GMM'中的一个
-	Returns:
-	  一个列表，包含该算法要调的参数的名称。
-	  要求列表大小和神经网络输出数目总数保持一致，顺序也和神经网络输出保持一致
-	'''
-	return ['a']
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 15:22:46 2019
 
+@author: 陈泊舟
+"""
+
+from sklearn.svm import SVC
+from sklearn.linear_model import ElasticNet
+from sklearn.mixture import GaussianMixture as GMM
+from sklearn.metrics import adjusted_rand_score
+from sklearn.model_selection import cross_val_score
+import os
+params_path = os.path.realpath(__file__).replace('\system\FutherOpt.py','') + os.sep + 'knowledge'
+# + os.sep + 'KnowledgePrepare.py'
+import sys
+sys.path.append(params_path)
+import knowledge.KnowledgePrepare
+'''
+均在fin_local_supreme函数刚开始时初始化
+'''
+global data#保存用户数据
+global last_param_list#保存上一次参数
+global tmp_param_list#保存最新参数
+global algorithm_type#取值1，2，3
+global tree#存储误差绝对值和segment_tree
+'''
+定义误差常量
+'''
+EPSILON = 0.001
+
+def find_local_supreme(dataset, alg_name, predicted_param):
+    '''
+    在神经网络给出结果的基础上，寻找局部极值，进一步优化超参数
+    Parameters:
+      dataset - 一个用户数据集，类型为pandas.DataFrame，每行一个样本，第一行是属性名称，第二行起是数据
+      alg_name - 待调参算法名称，String类型，'SVM','ElasticNet','GMM'中的一个
+      predicted_param - 神经网络的预测结果，一个列表，包含各个参数预测的值
+    Returns:
+      一个列表，包含各个参数的最终优化结果，要求参数次序和predicted_param相同
+    初始化全局变量
+    '''
+    global data#保存用户数据
+    data = dataset
+    global last_param_list#保存上一次参数
+    last_param_list = [-1000000 for x in range(len(predicted_param))]
+    global tmp_param_list#保存最新参数
+    tmp_param_list = predicted_param
+    global algorithm_type#取值1，2，3
+    if alg_name == 'SVM':
+        algorithm_type = 1
+    if alg_name == 'ElasticNet':
+        algorithm_type = 2
+    if alg_name == 'GMM':
+        algorithm_type = 3
+    global tree#存储误差绝对值和segment_tree
+    tree = [1000000 for x in range(4 * len(predicted_param))]
+    tree_init(1,len(predicted_param),1)
+    #begin running
+    func()
+    # return predicted_param
+    return tmp_param_list
+
+
+'''
+建立测试环境
+返回正确率
+'''
+def env():
+    if algorithm_type == 1:
+        return env1()
+    if algorithm_type == 2:
+        return env2()
+    if algorithm_type == 3:
+        return env3()
+    return
+'''
+SVM
+'''
+def env1():
+    params = {}
+    names = knowledge.KnowledgePrepare.get_param_name('SVM')
+    lenth = len(names)
+    for i in range(lenth):
+        params[names[i]] = tmp_param_list[i]
+    return svm_cross_validation(data,params)
+    
+'''
+ElasticNet
+'''
+def env2():
+    params = {}
+    names = knowledge.KnowledgePrepare.get_param_name('ElasticNet')
+    lenth = len(names)
+    for i in range(lenth):
+        params[names[i]] = tmp_param_list[i]
+    return elastic_net_cross_validation(data,params)
+'''
+GMM
+'''
+def env3():
+    params = {}
+    names = knowledge.KnowledgePrepare.get_param_name('GMM')
+    lenth = len(names)
+    for i in range(lenth):
+        params[names[i]] = tmp_param_list[i]
+    return gmm_score(data,params)
+'''
+多个参数调优
+'''
+def func(l,r,o):
+    if l == r:
+        return
+    if l + 1 == r:
+        func2(l,r)
+        return
+    mid = (l + r) / 2
+    while True:
+        func(l,mid,2 * o)
+        if check(l,r,o) == True:
+            break
+        func(mid + 1,r,2 * o + 1)
+        if check(l,r,o) == True:
+            break
+    return
+
+'''
+两个参数调优
+'''
+def func2(l,r):
+    hill_climb(l,r)
+    global last_param_list
+    global tmp_param_list
+    x = abs(tmp_param_list[l] - last_param_list[l])
+    y = abs(tmp_param_list[r] - last_param_list[r])
+    update(1,len(tmp_param_list),1,l,x)
+    update(1,len(tmp_param_list),1,r,y)
+    last_param_list = tmp_param_list
+    return
+
+'''
+单点set
+'''
+def update(l,r,o,index,value):
+    if l == r:
+        tree[o] = value
+        return
+    mid = (l + r) / 2
+    if index <= mid and index >= l:
+        update(l,mid,2 * 0,index,value)
+    if index > mid and index <= r:
+        update(mid + 1,r,2 * o + 1,index,value)
+    tree[o] = tree[2 * o] + tree[2 * o + 1]
+    return
+'''
+初始化树结构
+'''
+def tree_init(l,r,o):
+    if l == r:
+        tree[o] = 100
+        return
+    mid = (l + r) / 2
+    tree_init(l,mid,2 * o)
+    tree_init(mid + 1,r,2 * o + 1)
+    tree[o] = tree[2 * o] + tree[2 * o + 1]
+    return
+'''
+区间check
+'''
+def check(l,r,o):
+    if tree[o] <= (r - l + 1) * EPSILON:
+        return True
+    return False
+
+'''
+爬山法
+'''
+def hill_climb(l,r):
+    func1(l)
+    func1(r)
+    func1(l)
+    func1(r)
+    func1(l)
+    func1(r)
+    return
+'''
+一元爬山法
+'''
+def func1(index):
+    stride = 10
+    x = tmp_param_list[index]
+    while stride >= 1:
+        tmp_param_list[index] = x - stride
+        a = env()
+        tmp_param_list[index] = x
+        b = env()
+        tmp_param_list[index] = x + stride
+        c = env()
+        if a > b:
+            x = x - stride
+        elif c > b:
+            x = x + stride
+        else:
+            stride = stride / 2
+    return
+
+def svm_cross_validation(dataset,params):
+    '''
+    SVM交叉验证
+    Parameters:
+      待跑SVM的数据集，格式：pandas.DataFrame
+    Returns:
+      数据集10折交叉验证得分，使用默认的SVM超参数，评估标准：正确率
+    '''
+    y = dataset.pop('Label')
+    X = dataset
+
+    model = SVC(**params)
+    scores = cross_val_score(model, X, y, cv=10)
+
+    ret = scores.mean()
+    return ret
+
+def elastic_net_cross_validation(dataset,params):
+    '''
+    ElasticNet交叉验证
+    Parameters:
+      待运行ElasticNet的数据集，格式：pandas.DataFrame
+    Returns:
+      数据集10折交叉验证得分，使用默认的ElasticNet超参数，评估标准：r2
+    '''
+
+    y = dataset.pop('Label')
+    X = dataset
+
+    model = ElasticNet(**params)
+    model.fit(X, y)
+    scores = cross_val_score(model, X, y, cv=10, scoring='r2')
+
+    ret = scores.mean()
+    return ret
+
+def gmm_score(dataset,params):
+    '''
+    GMM聚类效果评估
+    Parameters:
+      待运行GMM的数据集，格式：pandas.DataFrame
+    Returns:
+      数据集聚类结果，使用已知的簇的个数，其他超参数使用默认值，评估标准：adjusted_rand_score
+    '''
+    y_true = dataset.pop('Label')
+    X = dataset
+
+    y_pred = GMM(**params).fit_predict(X)
+
+    ret = adjusted_rand_score(y_true, y_pred)
+    return ret
